73a0b4497ac2973f7e0da874e4a17d524193afd2
Malte <malte.wiethoff@t-online.de>
2025-03-12 23:33:00 +0100

    added benchmarking function for policies

--- utils.py
+++ utils.py
@@ -16,3 +16,29 @@ def print_V(policy, env):
     for y in range(env.height):
         print(" ".join(grid[y]))
     print("\n")
+
+def test_policy(policy, env, test_all_starts=True):
+    """runs the game with a given policy and then returns a success ratio"""
+    
+    if test_all_starts:
+        starts = range(env.width)
+    else:
+        starts = [env.width // 2]
+
+    success = 0
+    reward = 0
+
+    for start_x in starts:
+        done = False
+        state = env.reset(start_x)
+
+        while not done:        
+            state, reward, done = env.get_next_state(state, policy[state])
+
+        if reward > 0:
+            success += 1
+
+    print(f"level completion: {success}/{len(starts)}")
+    return success/len(starts)
+
+        
