540e7fba7192bcee25ee768b6fef4d33829ea887
Malte <malte.wiethoff@t-online.de>
2025-03-13 01:15:37 +0100

    started working on double q learning

--- q_learning.py
+++ q_learning.py
@@ -21,11 +21,34 @@ def q_learning(env: MDPGame, episodes, alpha, gamma, epsilon):
 
     return Q
 
+def double_q_learning(env: MDPGame, episodes, alpha, gamma, epsilon):
+    Q1 = defaultdict(lambda: defaultdict(lambda: 0)) # Q(s, a)
+    Q2 = defaultdict(lambda: defaultdict(lambda: 0))
+
+    for episode in range(episodes):
+        state = env.reset()
+        done = False
+
+        while not done:
+            action = n_epsilon_greedy_policy([Q1, Q2], state, epsilon) # this is necessary because summing Q1 and Q2 every interation results in a massive slow down
+
+            next_state, reward, done = env.get_next_state(state, action)
+            if random.uniform(0, 1) < .5:
+                best_action = max(Q1[next_state], key=Q1[next_state].get) if len(Q1[next_state]) > 0 else 0
+                Q1[state][action] += alpha * (reward + gamma * Q1[next_state][best_action] - Q1[state][action])
+            else:
+                best_action = max(Q2[next_state], key=Q2[next_state].get) if len(Q2[next_state]) > 0 else 0
+                Q2[state][action] += alpha * (reward + gamma * Q2[next_state][best_action] - Q2[state][action])
+
+            state = next_state
+
+    return sum_Q([Q1, Q2])
+
 if __name__ == "__main__":
     env = MDPGame(random_x=True)
-    env.load_level(hard_level)
+    # env.load_level(hard_level)
 
-    Q = q_learning(env, episodes=10000, alpha=0.1, gamma=0.99, epsilon=0.1)
+    Q = double_q_learning(env, episodes=10000, alpha=0.1, gamma=0.99, epsilon=0.1)    
     V = reduce_Q_to_V(Q, env)
 
     print_V(V, env)
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
-import random 
+import random
+from collections import defaultdict
 
 def print_V(policy, env):
     """Prints the policy as a visual grid."""
@@ -48,12 +49,36 @@ def epsilon_greedy_policy(Q, state, epsilon):
     else:
         # returns the key of the maximum value in the dictionary 
         return max(Q[state], key=Q[state].get) if state in Q else 0
+    
+def n_epsilon_greedy_policy(Qs, state, epsilon):
+    """applies epsilon greedy strategy to the sum of multiple Qs"""
+    if random.uniform(0, 1) < epsilon:
+        return random.randint(-1, 1) # choose random action
+    else:
+        summed_Q = defaultdict(lambda: 0)
+        for actions in [Q[state] for Q in Qs]:
+            for key in actions.keys():
+                summed_Q[key] += actions[key]
+
+        return max(summed_Q[state], key=summed_Q[state].get) if state in summed_Q else 0
+    
+def sum_Q(Qs):
+    """returns the sum of Q-values of a list of state action spaces"""
+    summed_Q = defaultdict(lambda: defaultdict(lambda: 0))
+
+    for Q in Qs:
+        for state in Q.keys():
+            for action in Q[state].keys():
+                summed_Q[state][action] += Q[state][action]
+
+    return summed_Q
+
 
 def reduce_Q_to_V(Q, game):
     """reduces Q to V by choosing the action with the highes Q value"""
     V = {}
     for state in [(y, x) for y in range(game.height) for x in range(game.width)]:
-        V[state] = max(Q[state], key=Q[state].get) if state in Q else 0
+        V[state] = max(Q[state], key=Q[state].get) if state in Q and len(Q[state]) > 0 else 0
 
     return V
         
